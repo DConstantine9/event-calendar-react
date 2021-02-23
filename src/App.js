467c0e7f2d73ba73bad944dc5b225d410e795b1f
DConstantine9 <constantinedeniskkin@gmail.com>
2021-02-23 18:58:09 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DatePickerExample from "./Calendar.js"
+
+function App() {
+  return (
+    <div className="App">
+      <DatePickerExample />
+    </div>
+  );
+}
+
+export default App;
